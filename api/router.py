2ca7a37d6d2be657c4c66405a7e6f2161c07dd7a
SAVAGEAKALIA <akaliasaviour@gmail.com>
2025-02-14 04:18:44 +0100

    added github actions

--- api/router.py
+++ api/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from api.routes import books
+from api.routes import books, health
 
 api_router = APIRouter()
+# api_router.include_router(books.router, prefix="/books", tags=["books"])
 api_router.include_router(books.router, prefix="/books", tags=["books"])
+# api_router.include_router(health.router, tags=["health"])
